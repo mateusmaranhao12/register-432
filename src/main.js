4b339648c3cdad17e46eb00c02b7c730f1af3c1f
Mateus Maranhão <feradourada@gmail.com>
2023-02-20 11:17:38 -0300

    Init

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "bootstrap";
+import "./assets/app.scss";
+
+import $ from "jquery";
+window.jQuery = $;
+window.$ = $
 
 createApp(App).use(router).mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 ml-auto mr-auto offset-md-4">
+          <div class="card my-4">
+            <div class="card-heading">
+              <h4 class="h card-title bg-info p-3 text-center text-white">
+                Admin Login
+              </h4>
+            </div>
+            <div class="card-body">
+              <form action="">
+                <div class="form-row">
+                  <div class="form-group col-md-12 mx-2">
+                    <label for="user">Usuário</label>
+                    <input type="text" placeholder="Usuário" id="usuario" class="form-control mb-3" name="usuario"/>
+                  </div>
+                  <div class="form-group col-md-12 mx-2">
+                    <label for="user">Senha</label>
+                    <input type="password" placeholder="Senha" id="senha" class="form-control" name="senha"/>
+                  </div>
+                </div>
+                <div class="form-row">
+                  <div class="form-group col-md-12">
+                    <button class="btn btn-primary float-end mt-2">Fazer login</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+
+}
 </script>
+
+<style>
+
+</style>
